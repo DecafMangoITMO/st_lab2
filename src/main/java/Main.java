5e007cbc223741131a84545e6c3e1ceb2a999103
DecafMango <DecafMangoITMO@yandex.ru>
2025-03-04 16:03:50 +0300

    complete task functions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,9 @@
-import function.Function;
-import function.task.LogarithmicFunction;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        Function f = new LogarithmicFunction();
-        System.out.println(f.calculate(5, Double.MIN_VALUE));
+
     }
 
 }
--- src/main/java/function/task/TrigonometricFunction.java
+++ src/main/java/function/task/TrigonometricFunction.java
@@ -1,11 +1,20 @@
 package function.task;
 
 import function.Function;
+import function.impl.*;
 
 public class TrigonometricFunction implements Function {
 
     @Override
     public double calculate(double x, double epsilon) {
-        return 0;
+        Function sin = new SinFunction();
+        Function cos = new CosFunction(sin);
+        Function tan = new TanFunction(sin, cos);
+        Function cot = new CotFunction(sin, cos);
+        Function sec = new SecFunction(cos);
+        Function csc = new CscFunction(sin);
+
+        return (((((tan.calculate(x, epsilon) / sec.calculate(x, epsilon)) * cot.calculate(x, epsilon)) - sec.calculate(x, epsilon)) - csc.calculate(x, epsilon)) * ((csc.calculate(x, epsilon) + sin.calculate(x, epsilon)) / sec.calculate(x, epsilon)));
     }
+
 }
